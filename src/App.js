74a24de7bb06ee92900d2484290de788963c4789
Marie Muramatsu <muramatsumarie@gmail.com>
2023-08-14 10:26:48 +0900

    :lipstick: create addBar UI using MUI

--- src/App.js
+++ src/App.js
@@ -5,18 +5,38 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 const label = { inputProps: { '': 'Checkbox demo' } };
+const theme = {
+  spacing: 8,
+}
+
 
 function AddToDo(){
 
   const {handleClick} = useAddTask;
 
   return (
-    <div className="addBar"> 
-    <TextField id="outlined-basic" label="入力" variant="outlined" className="addText"/>
-    <Button variant="contained" onClick={handleClick}>Add</Button>
-    </div>
+    <Container maxWidth="sm">
+      <Grid container spacing={1}>
+      <Grid item md={8}>
+        <TextField id="outlined-basic" label="入力" variant="outlined" size="small"/>
+      </Grid>
+      <Grid item md={3}>
+        <Box sx={{mr:2}}>
+          <Button variant="contained" onClick={handleClick}>Add</Button>
+
+        </Box>
+        
+      </Grid>
+
+    </Grid>
+    </Container>
+    
+      
   )
 }
 
@@ -54,6 +74,7 @@ export default function App(){
   return (
     <div className='App'>
       <h1>To Do App</h1>
+      <br></br>
       <AddToDo />
       <TaskList/>
 
